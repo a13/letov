bd9f083dae04ad1530cb8786ea90ee2a8ed628ed
DK <beerandhot@gmail.com>
2016-07-20 16:43:47 +0300

    simplified letov

--- src/letov/app.clj
+++ src/letov/app.clj
@@ -9,24 +9,35 @@
     (partial remove str/blank?)
     str/split-lines
     slurp
-    #(str "data/list_" % ".txt"))
+;;    #(str "data/list_" % ".txt"))
+    #(str "https://raw.githubusercontent.com/a13/letov/master/data/list_" % ".txt"))
    (range 1 5)))
 
-(defn verse
+(defn- verse
   []
-  (cons '("О-о-о-о! Моя оборона!")
-        (repeatedly 4 #(map rand-nth files-content))))
+  (str/join \newline
+   (cons "О-о-о-о! Моя оборона!"
+         (repeatedly 4 #(str/join \space (map rand-nth files-content))))))
+
+(defn- char-dly
+  [char]
+  (case char
+    \space 100
+    \newline 300
+    40))
+
+(defn- print-verse
+  [verse]
+  (doseq [char verse]
+    (print char)
+    (flush)
+    (Thread/sleep (char-dly char)))
+  (newline)
+  (newline))
 
 (defn -main [& args]
   (while true
-    (doseq [line (verse)]
-      (doseq [char (str/join " " line)]
-        (print char)
-        (flush)
-        (Thread/sleep 40))
-      (Thread/sleep 200)
-      (newline))
-    (newline))
+    (print-verse (verse)))
   (newline))
 
 
